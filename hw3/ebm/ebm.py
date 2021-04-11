adfe7c0b255b9ce8a735f7486e0f0e5d7c95705b
zhourunlong <zhourunlongvector@gmail.com>
2021-04-11 11:43:09 +0800

    finish ebm

--- hw3/ebm/ebm.py
+++ hw3/ebm/ebm.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from collections import deque
-
+import random
 
 class MlpBackbone(nn.Module):
     def __init__(self, input_shape, hidden_size, activation=nn.functional.leaky_relu):
@@ -27,8 +27,13 @@ class MlpBackbone(nn.Module):
 
 
 class Trainer(object):
-    def __init__(self, model: MlpBackbone, device, buffer_size, langevin_k, langevin_noise_std, langevin_lr,
-                 replay_p, lr, l2_coef=0., proj_norm=None):
+    def __init__(self, model: MlpBackbone, device, seed, buffer_size, langevin_k, langevin_noise_std, langevin_lr, replay_p, lr, l2_coef, proj_norm=None):
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+
         self.model = model
         self.device = device
         self.replay_buffer = deque(maxlen=buffer_size)
@@ -48,32 +53,62 @@ class Trainer(object):
         :param x: initial negative sample
         :return: the resulting negative sample
         '''
-        # TODO: implement langevin dynamics over the model
-        return x
+        x.requires_grad_(True)
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        for i in range(self.langevin_k):
+            out = self.model(x)
+            out.sum().backward()
+
+            x.data.add_(x.grad.data, alpha = -self.langevin_lr)
+            x.grad.zero_()
+
+            noise = torch.randn(x.shape[0], 1, 28, 28, device=self.device)
+            noise.normal_(0, self.langevin_noise_std)
+
+            x.data.add_(noise)
+
+            x.data.clamp_(0, 1)
+
+        for p in self.model.parameters():
+            p.requires_grad = True
+
+        return x.detach()
 
     def init_negative(self, batch_size):
         '''
         :param batch_size:
         :return: initial negative samples, a tensor of shape (batch_size,) + self.model.input_shape
         '''
-        # TODO: initialize negative samples here.
-        #  Sample from random noise with some probability; sample from self.replay_buffer otherwise.
-        return x
+        if len(self.replay_buffer) < 1:
+            return torch.rand(batch_size, 1, 28, 28, device=self.device)
+        
+        n_replay = (np.random.rand(batch_size) < self.replay_p).sum()
 
-    def train_step(self, batch_x: torch.Tensor):
-        self.x_pos = batch_x
-        self.x_neg_init = x_neg_init = self.init_negative(batch_x.shape[0])
-        self.x_neg = x_neg = self.langevin_dynamic(x_neg_init)
-        # TODO: write training objective here.
-        loss = 0  # This is just a placeholder.
+        replay_sample = random.sample(self.replay_buffer, n_replay)
+        replay_sample = torch.stack(replay_sample)
+        random_sample = torch.rand(batch_size - n_replay, 1, 28, 28, device=self.device)
 
+        return torch.cat([replay_sample, random_sample], 0)
+
+    def train_step(self, pos_img: torch.Tensor):
+        neg_init = self.init_negative(pos_img.shape[0])
+        neg_img = self.langevin_dynamic(neg_init)
+        
         self.optimizer.zero_grad()
+
+        pos_out = self.model(pos_img)
+        neg_out = self.model(neg_img)
+
+        loss = pos_out - neg_out + self.l2_coef * (pos_out ** 2 + neg_out ** 2)
+        loss = loss.mean()
         loss.backward()
+
         self.optimizer.step()
-        # Fill replay buffer
-        self.replay_buffer.extend([x_neg[i] for i in range(x_neg.shape[0])])
-        # TODO: you may return anything you like for debugging
-        return
+
+        self.replay_buffer.extend(neg_img)
+        return loss
 
     def inpainting(self, corrupted: torch.Tensor, mask):
         '''
@@ -82,8 +117,28 @@ class Trainer(object):
                      ``0'' positions indicate ground truth pixels, which should not be changed during inpainting.
         :return: recovered images with the same size as ``corrupted''.
         '''
-        recovered = corrupted
-        return recovered
+
+        bsz = corrupted.shape[0]
+
+        x = []
+        for j in range(bsz):
+            x.append(corrupted[j].clone())
+            x[j].requires_grad_(True)
+        for p in self.model.parameters():
+            p.requires_grad = False
+
+        for i in range(self.langevin_k):
+            for j in range(bsz):
+                out = self.model(x[j])
+                out.backward()
+                x[j].data.add_(x[j].grad.data * mask[j], alpha = -self.langevin_lr)
+                x[j].grad.zero_()
+                x[j].data.clamp_(0, 1)
+
+        for p in self.model.parameters():
+            p.requires_grad = True
+
+        return torch.stack(x).detach()
 
     def save(self, save_path):
         save_dict = {'model': self.model.state_dict(),
--- hw3/ebm/main.py
+++ hw3/ebm/main.py
@@ -1,11 +1,13 @@
-import argparse, time
-import torch
+import argparse, time, logging, os, sys
+import torch, torchvision
 import numpy as np
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from ebm import MlpBackbone, Trainer
-
+from collections import deque
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def main(args):
     np.random.seed(args.seed)
@@ -18,27 +20,45 @@ def main(args):
         dataloader = DataLoader(dataset, args.batch_size, shuffle=True, drop_last=True)
     else:
         raise NotImplementedError
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cuda:"+str(args.gpuid) if torch.cuda.is_available() else "cpu")
     # Model
     model = MlpBackbone(input_shape=(1, 28, 28), hidden_size=512)
     model.to(device)
     # EBM trainer
-    trainer = Trainer(model, device, args.buffer_size, args.langevin_k, args.langevin_noise_std,
-                      args.langevin_lr, args.replay_p, args.lr, args.l2_coef, args.proj_norm)
+    trainer = Trainer(model, device, args.seed, args.buffer_size, args.langevin_k, args.langevin_noise_std, args.langevin_lr, args.replay_p, args.lr, args.l2_coef, args.proj_norm)
+
+    log_format = '%(asctime)s %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format=log_format, datefmt='%m/%d %I:%M:%S %p')
+    fh = logging.FileHandler(os.path.join(args.logdir, 'log.txt'))
+    fh.setFormatter(logging.Formatter(log_format))
+    logging.getLogger().addHandler(fh)
 
     if not args.play:
         # Training.
         for epoch in range(args.num_epochs):
-            for data, label in dataloader:
-                # TODO: add logging, saving, or anything else you believe useful
-                # trainer.train_step(data.to(device))
-                pass
+            cnt = 0
+            running_loss = 0
+            for pos_img, label in dataloader:
+                loss = trainer.train_step(pos_img.to(device))
+
+                cnt += 1
+                running_loss += loss
+                
+                if cnt % args.log_interval == 0:
+                    logging.info("epoch %d, average loss %f" % (epoch + 1, running_loss / cnt))
+            
+            save_path = os.path.join(args.logdir, "ebm_checkpoint_{}.pth".format(epoch))
+            trainer.save(save_path)
+            logging.info("saved checkpoint to {}".format(save_path))
+
     else:
         # Play around with a trained model.
         assert args.load_dir is not None, "You must specify load_dir."
         trainer.load(args.load_dir)
         all_mse = []
         initial_mse = []
+        cnt = 0
         for data, label in dataloader:
             broken_data = torch.clone(data)
             # Corrupt the rows 0, 2, 4, ....
@@ -48,32 +68,53 @@ def main(args):
             broken_data = broken_data.to(device)
             broken_data[mask] += 0.3 * torch.randn(*broken_data.shape, device=device)[mask]
             broken_data = torch.clip(broken_data, 0., 1.)
-            # TODO: just an example. You may add more visualization or anything else.
+
             recovered_img = trainer.inpainting(broken_data.to(device), mask)
-            mse = np.mean((data.numpy() - recovered_img) ** 2, axis=(1, 2, 3))
+
+            img_queue = []
+            for i in range(data.shape[0]):
+                img_queue.append(broken_data[i])
+                img_queue.append(recovered_img[i])
+            img = torchvision.utils.make_grid(torch.stack(img_queue).cpu(), nrow = 16).numpy()
+            plt.imshow(np.transpose(img, (1,2,0)))
+            save_path = os.path.join(args.logdir, "denoise_batch{}.jpg".format(cnt))
+            plt.savefig(save_path)
+            logging.info("saved {}".format(save_path))
+
+            mse = np.mean((data.numpy() - recovered_img.cpu().numpy()) ** 2, axis=(1, 2, 3))
             all_mse.extend(mse.tolist())
             mse = np.mean((data.numpy() - broken_data.cpu().numpy()) ** 2, axis=(1, 2, 3))
             initial_mse.extend(mse.tolist())
-        print("mean mse", np.mean(all_mse), "std", np.std(all_mse), "max", np.max(all_mse), "min", np.min(all_mse))
-        print("mean mse", np.mean(initial_mse), "std", np.std(initial_mse), "max", np.max(initial_mse), "min", np.min(initial_mse))
+
+            cnt += 1
+        logging.info("all:     mean mse %f std %f max %f min %f" % (np.mean(all_mse), np.std(all_mse), np.max(all_mse), np.min(all_mse)))
+        logging.info("initial: mean mse %f std %f max %f min %f" % (np.mean(initial_mse), np.std(initial_mse), np.max(initial_mse), np.min(initial_mse)))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--dataset", choices=["mnist"], default="mnist")
-    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--seed", type=int, default=2018011309)
     parser.add_argument("--logdir", type=str, default=None)
     parser.add_argument("--play", action="store_true", default=False)
     parser.add_argument("--load_dir", type=str, default=None)
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--buffer_size", type=int, default=10000)
-    parser.add_argument("--langevin_k", type=int, default=1)
-    parser.add_argument("--langevin_noise_std", type=float, default=1)
-    parser.add_argument("--langevin_lr", type=float, default=1)
+    parser.add_argument("--langevin_k", type=int, default=60)
+    parser.add_argument("--langevin_noise_std", type=float, default=0.005)
+    parser.add_argument("--langevin_lr", type=float, default=10)
     parser.add_argument("--replay_p", type=float, default=0.95)
-    parser.add_argument("--lr", type=float, default=1)
-    parser.add_argument("--l2_coef", type=float, default=0)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--l2_coef", type=float, default=1)
     parser.add_argument("--proj_norm", type=float, default=None)
-    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--num_epochs", type=int, default=100)
+    parser.add_argument("--log_interval", type=int, default=50)
+    parser.add_argument("--gpuid", default=0, type=int)
     args = parser.parse_args()
+
+    if args.logdir is None:
+        args.logdir = "Models-{}".format(time.strftime("%Y%m%d-%H%M%S"))
+    os.makedirs(args.logdir, exist_ok=True)
+    print("Experiment dir : {}".format(args.logdir))
+
     main(args)
