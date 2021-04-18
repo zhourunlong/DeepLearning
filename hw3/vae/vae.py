54cf9af7d167324f6bba20cf3a5593766234bfb6
zhourunlong <zhourunlongvector@gmail.com>
2021-04-18 11:03:38 +0800

    finish vae

--- hw3/vae/vae.py
+++ hw3/vae/vae.py
@@ -1,13 +1,16 @@
-import argparse
-import torch
+import argparse, sys, os
+import torch, torchvision
 import torch.optim as optim
 import numpy as np
 from torch import nn as nn
 from torch.utils.data import DataLoader
+from torch.nn import functional as F
 from torchvision.datasets import MNIST
 from torchvision import transforms
-import os, time
-
+import os, time, logging
+from tqdm import tqdm
+from mnist_classifier import MnistClassifier
+import matplotlib.pyplot as plt
 
 class CVAE(nn.Module):
     def __init__(self, img_size, label_size, latent_size, hidden_size=256):
@@ -52,8 +55,16 @@ class CVAE(nn.Module):
         :param batch_label: a tensor of shape (batch_size, self.label_size)
         :return: a batch of latent code of shape (batch_size, self.latent_size)
         '''
-        # TODO: compute latent z from images and labels
-        return None  # Placeholder.
+
+        input = torch.cat((self.enc_img_fc(batch_img.view(batch_img.shape[0], -1)), self.enc_label_fc(batch_label)), dim = 1)
+        output = self.encoder(input)
+        mu = self.z_mean(output)
+        logsigma = self.z_logstd(output)
+
+        dz = torch.randn((batch_img.shape[0], self.latent_size), device=batch_img.device)
+        z = mu + torch.exp(0.5 * logsigma) * dz
+
+        return z, mu, logsigma
 
     def decode(self, batch_latent, batch_label):
         '''
@@ -61,7 +72,15 @@ class CVAE(nn.Module):
         :param batch_label: a tensor of shape (batch_size, self.label_size)
         :return: reconstructed results
         '''
-        return None  # Placeholder.
+        input = torch.cat((self.dec_latent_fc(batch_latent), self.dec_label_fc(batch_label)), dim = 1)
+        output = self.decoder(input)
+
+        return output
+
+    def forward(self, batch_img, batch_label):
+        z, mu, logsigma = self.encode(batch_img, batch_label)
+        x = self.decode(z, batch_label)
+        return x, mu, logsigma
 
     def sample(self, batch_latent, batch_label):
         '''
@@ -70,9 +89,10 @@ class CVAE(nn.Module):
         :return: a tensor of size (batch_size, C, H, W), each value is in range [0, 1]
         '''
         with torch.no_grad():
-            # TODO: get samples from the decoder.
-            pass
-        return None  # Placeholder.
+            input = torch.cat((self.dec_latent_fc(batch_latent), self.dec_label_fc(batch_label)), dim = 1)
+            output = self.decoder(input)
+
+            return output.clamp(0, 1).view(-1, 1, 28, 28)
 
 
 #########################
@@ -87,6 +107,10 @@ def generate_samples(cvae, n_samples_per_class, device):
     return samples
 #########################
 
+def criterion(recon_x, x, mu, logsigma):
+    Lrc = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction = "sum")
+    DKL = 0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp())
+    return Lrc - DKL
 
 def main(args):
     np.random.seed(args.seed)
@@ -96,45 +120,112 @@ def main(args):
     # Load dataset
     if args.dataset == "mnist":
         dataset = MNIST(root="../data",
-                        transform=transforms.ToTensor(),  # TODO: you may want to tweak this
+                        transform=transforms.ToTensor(),
                         train=not args.eval)
         dataloader = DataLoader(dataset, args.batch_size, shuffle=True, drop_last=True)
     else:
         raise NotImplementedError
 
     # Configure
-    logdir = args.logdir if args.logdir is not None else "/tmp/cvae_" + time.strftime('%Y-%m-%d-%H-%M-%S')
-    os.makedirs(logdir, exist_ok=True)
+    logdir = args.logdir
 
     label_dim = 10
     img_dim = (1, 28, 28)
     latent_dim = 100
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:" + str(args.gpuid) if torch.cuda.is_available() else "cpu")
     cvae = CVAE(img_dim, label_dim, latent_dim)
     cvae.to(device)
     optimizer = optim.Adam(cvae.parameters(), lr=args.lr)
+
+    validator = torch.load("mnist_classifier.pth", map_location=device)
 
     if not args.eval:
         for name, param in cvae.named_parameters():
             print(name, param.shape)
         prior = torch.distributions.Normal(0, 1)
+        best_acc = 0
+        
         for epoch in range(args.num_epochs):
-            # TODO: Training, logging, saving, visualization, etc.
-            pass
+            cvae.train()
+            train_loss = 0
+            idx = 0
+
+            pbar = tqdm(total=len(dataloader.dataset))
+            for batch_idx, (data, label) in enumerate(dataloader):
+                idx = batch_idx
+                data = data.to(device)
+                label = F.one_hot(label.to(device), 10).float()
+                optimizer.zero_grad()
+
+                x, mu, logsigma = cvae(data, label)
+                
+                loss = criterion(x, data, mu, logsigma)
+                train_loss += loss.item()
+
+                loss.backward()
+                optimizer.step()
+
+                pbar.update(data.size(0))
+                pbar.set_description('Train, loss: {:.6f}'.format(train_loss / (batch_idx + 1)))
+                pbar.set_postfix(lr=args.lr)
+
+            pbar.close()
+
+            logging.info('Epoch {}. Train, loss: {:.6f}'.format(epoch, train_loss / (idx + 1)))
+
+            save_path = os.path.join(args.logdir, "models/model{}.pth".format(epoch))
+            torch.save(cvae, save_path)
+            logging.info('Saving to {}'.format(save_path))
+
+            img_queue = generate_samples(cvae, 10, device)['imgs']
+            img_grid = torchvision.utils.make_grid(img_queue.cpu(), nrow = 10).numpy()
+            plt.imshow(np.transpose(img_grid, (1,2,0)))
+            save_path = os.path.join(args.logdir, "images/valid_batch{}.jpg".format(epoch))
+            plt.savefig(save_path)
+
+            num_per_class = 1024
+            valid_data_queue = generate_samples(cvae, num_per_class, device)
+            valid_data = valid_data_queue['imgs'].to(device)
+            valid_label = valid_data_queue['labels'].to(device)
+
+            acc = 0
+
+            pbar = tqdm(total = num_per_class // 128 * 10)
+            for i in range(num_per_class // 128 * 10):
+                data = valid_data[i * 128: (i + 1) * 128, :, :, :]
+                data = transforms.Normalize((0.1307,), (0.3081,))(data)
+                label = F.one_hot(valid_label[i * 128: (i + 1) * 128], 10).float()
+                
+                with torch.no_grad():
+                    output = validator(data)
+                    pred = output.argmax(dim=1, keepdim = True)
+                    acc += pred.eq(valid_label[i * 128: (i + 1) * 128].view_as(pred)).sum().item()
+
+                pbar.update(1)
+                pbar.set_description('Valid, acc: {:.6f}'.format(acc / (128 * (i+ 1))))
+                pbar.set_postfix(lr=args.lr)
+            
+            pbar.close()
+
+            acc /= num_per_class * 10
+            logging.info('Valid, acc: {:.6f}'.format(acc))
+
+            if acc > best_acc:
+                best_acc = acc
+                save_path = os.path.join(args.logdir, "models/best.pth")
+                torch.save(cvae, save_path)
+                logging.info('Best acc! Saving to {}'.format(save_path))
+
     else:
         assert args.load_path is not None
-        checkpoint = torch.load(args.load_path, map_location=device)
-        cvae.load_state_dict(checkpoint['model'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        cvae.eval()
+        cvae = torch.load(args.load_path, map_location=device)
         samples = generate_samples(cvae, 1000, device)
         torch.save(samples, "vae_generated_samples.pt")
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--seed", type=int, default=2018011309)
     parser.add_argument("--dataset", choices=["mnist"], default="mnist")
     parser.add_argument("--lr", type=float, default=2e-4)
     parser.add_argument("--batch_size", type=int, default=128)
@@ -142,5 +233,21 @@ if __name__ == "__main__":
     parser.add_argument("--logdir", type=str, default=None)
     parser.add_argument("--eval", action="store_true", default=False)
     parser.add_argument("--load_path", type=str, default=None)
+    parser.add_argument("--gpuid", type=int, default=0)
     args = parser.parse_args()
+
+    if args.logdir is None:
+        args.logdir = "Models-{}".format(time.strftime("%Y%m%d-%H%M%S"))
+    os.makedirs(args.logdir, exist_ok=True)
+    os.makedirs(os.path.join(args.logdir, "models"), exist_ok=True)
+    os.makedirs(os.path.join(args.logdir, "images"), exist_ok=True)
+    print("Experiment dir : {}".format(args.logdir))
+
+    log_format = '%(asctime)s %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format=log_format, datefmt='%m/%d %I:%M:%S %p')
+    fh = logging.FileHandler(os.path.join(args.logdir, 'log.txt'))
+    fh.setFormatter(logging.Formatter(log_format))
+    logging.getLogger().addHandler(fh)
+
     main(args)
